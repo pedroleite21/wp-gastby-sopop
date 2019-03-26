ebbfade67963e9a959ee68409402f915251d31fd
Pedro Leite <pedro.passos@pucrs.br>
2019-03-26 13:29:34 -0300

    Change site

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: "gatsby-source-wordpress",
       options: {
-        baseUrl: "quaseastronauta.000webhostapp.com/",
+        baseUrl: "sopop.xyz/",
         protocol: "http",
         hostingWPCOM: false,
         useACF: true,
