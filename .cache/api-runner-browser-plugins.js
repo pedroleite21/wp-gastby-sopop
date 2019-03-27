e57be1d47381536ca854d7e84728180f8df8b19d
Pedro Leite <pedro.passos@pucrs.br>
2019-03-27 09:33:07 -0300

    Delete cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('C:/Users/LIS/Desktop/wp-gastby-sopop/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
-      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
-    },{
-      plugin: require('C:/Users/LIS/Desktop/wp-gastby-sopop/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
